954418524368a0f6a3a9ee88c7399a8f637d6d7b
harshit <harshitgangwar51102@gmail.com>
2025-03-12 16:24:19 +0530

    adding link to go to assistants page in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <Button>Press Me</Button>
+      <Link href="/ai-assistants">
+        <Button>Press Me</Button>
+      </Link>
     </div>
   );
 }
